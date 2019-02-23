db69e14fb3936c19e80779e8771ce663df3297aa
ken <kirie115@gmail.com>
2019-02-24 00:01:06 +0900

    ①createアクションの実装および②成功時/失敗時にビューへメッセージが表示されるよう実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,25 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path, notice: "蔵書の登録が完了しました"
+    else
+      flash.now[:notice] = "蔵書の登録が失敗しました。もう一度やり直してください。"
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :author, :memo)
   end
 
 end
