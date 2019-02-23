b104eb158e8bc96096fa906d5d3aa28c66a93ac3
ken <kirie115@gmail.com>
2019-02-24 00:45:04 +0900

    検索に使用出来るカラムをnameのみに設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
   validates :name, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
